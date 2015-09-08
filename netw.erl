2e01082b173d8f1b2891cdabc9478193d277bcf6
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-09-08 11:54:22 +0300

    multiplex function added

--- netw.erl
+++ netw.erl
@@ -4,16 +4,16 @@
 
 
 -module(netw).
--export([gen_rand_net/1, setup/3
+-export([gen_rand_net/1, setup/2
 		]).
 
 -define(PORTS_NBR,4).
 -define(MAX_RAND_ATTEMPTS,5).
--define(MAX_LATENCY,100).
+-define(BPS,1000). % 1000 bit per second
 
 
 
-setup(N,Traff,Msg_len) ->
+setup(N,Traff) ->
 	random:seed(),
 	G = gen_rand_net(N),
 	Bs = lists:foldl(fun(B,Acc) -> [ {B, spawn(box,new,[B,Traff]) } |Acc] 
@@ -24,8 +24,8 @@ setup(N,Traff,Msg_len) ->
 						io:format("Links:~n~p~n",[Links]),
 						Neibs = [ begin
 									{B2,Bid}=lists:keyfind(B2,1,Bs),
-									{B2,P,Lat,Bid}
-								  end || {B2,P,Lat} <- Links ],
+									{B2,P,Bid}
+								  end || {B2,P} <- Links ],
 						Pid ! {neibs, Neibs} 
 					end, Bs),
 	
@@ -53,12 +53,12 @@ gen_rand_net(N) ->
 	M = round(0.3 *?PORTS_NBR*N + random:uniform(round(0.2*?PORTS_NBR*N))),
 	io:format("Generating ~w wires~n",[M]),
 
-	Net = dict:from_list([ {get_box(J),[]} || J <- lists:seq(1,N) ]),
+	Net = dict:from_list([ {box_name(J),[]} || J <- lists:seq(1,N) ]),
 	wire_boxes(N,M,Net).
 
 
 
-get_box(J) -> "box "++ integer_to_list(J).
+box_name(J) -> "box "++ integer_to_list(J).
 
 
 
@@ -68,8 +68,7 @@ wire_boxes(N,M,Net) ->
 		{Box1,Box2} ->
 			P1 = 1 + length(dict:fetch(Box1,Net)),
 			P2 = 1 + length(dict:fetch(Box2,Net)),
-			Lat = get_rand_latency(),
-			Net1 = dict:append(Box1,{Box2,P1,Lat},dict:append(Box2,{Box1,P2,Lat},Net)),
+			Net1 = dict:append(Box1,{Box2,P1},dict:append(Box2,{Box1,P2},Net)),
 			wire_boxes(N,M-1,Net1);
 		false ->
 			io:format("~p wires could not be used~n",[M]),
@@ -80,8 +79,8 @@ wire_boxes(N,M,Net) ->
 
 get_random_wire(_,_,?MAX_RAND_ATTEMPTS) -> false;
 get_random_wire(N,Net,Attempt) ->
-	B1 = get_box(random:uniform(N)),
-	B2 = get_box(random:uniform(N)),
+	B1 = box_name(random:uniform(N)),
+	B2 = box_name(random:uniform(N)),
 	case length(dict:fetch(B1,Net))>=?PORTS_NBR orelse length(dict:fetch(B2,Net))>=?PORTS_NBR of
 		true -> get_random_wire(N,Net,Attempt+1);
 		false -> case B1=:=B2 of
@@ -89,10 +88,6 @@ get_random_wire(N,Net,Attempt) ->
 					false ->{B1,B2}
 				end
 	end.
-
-
-
-get_rand_latency() -> random:uniform(?MAX_LATENCY div 2) + (?MAX_LATENCY div 2).
 
 
 
